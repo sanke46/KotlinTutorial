ea56e264177650e66477ac978960a31a69013f0b
sanke46 <shift.of.razum@gmail.com>
2017-06-08 17:15:02 +0300

    Kotlin Tutorial - first git

---
+++ src/Main.kt
@@ -0,0 +1,28 @@
+/**
+ * Created by ilafedoseev on 08.06.17.
+ */
+
+fun main(args : Array<String>) {
+    println("Hello world")
+
+    var letterChar : Char = 'A'
+    
+
+//    print("A is a Char : ${letterChar is Char}")
+
+//    if(true is Boolean){
+//        print("true is boolean\n")
+//    }
+
+//    val name = "Ilya"
+//
+//    var myAge = 22
+//
+//    var bigInt : Int = Int.MAX_VALUE
+//    var smallInt : Int = Int.MIN_VALUE
+//
+//    println("Biggest Int : " + bigInt)
+//    println("Smallest Int : $smallInt")
+
+
+}
